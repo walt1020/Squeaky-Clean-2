7f0efef64cad3cd39d41643a33e1b75c1f62a17f
walt1020 <noblebomba@gmail.com>
2018-02-15 18:05:48 -0500

    update db.js

--- back/config/db.js
+++ back/config/db.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+
+
+if (process.env.JAWSDB_URL) {
+	const db = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else(
+	const db = mysql.createConnection({
       host: "scdb.cx9vfnsthqml.us-east-2.rds.amazonaws.com",
       user: "master",
       password: "password",
       database: "scdb",
       port: 3306
-});
+	});
+)
 
 module.exports = db;
